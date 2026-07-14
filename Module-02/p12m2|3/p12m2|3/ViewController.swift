b1a3e57fb0f81940e003a262cb574b4131b393e8
Anton Tyurin <regul.t.86@gmail.com>
2026-07-14 21:32:39 +0500

    Module 2|3 completed

---
+++ Module-02/p12m2|3/p12m2|3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  p12m2|3
+//
+//  Created by Anton Tyurin on 14.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
